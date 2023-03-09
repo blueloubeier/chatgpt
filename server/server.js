91962712140cdae7899f1964005f6d0f3895b9bb
loubeier <you@example.com>
2023-03-09 17:01:54 +0000

    update

---
+++ server/server.js
@@ -0,0 +1,55 @@
+import express from 'express'
+import * as dotenv from 'dotenv'
+import cors from 'cors'
+import { Configuration, OpenAIApi } from 'openai'
+
+dotenv.config()
+
+const configuration = new Configuration({
+  apiKey: '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',
+});
+
+const openai = new OpenAIApi(configuration);
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+app.get('/', async (req, res) => {
+  res.status(200).send({
+    message: 'Hello from CodeX!'
+  })
+})
+
+app.post('/', async (req, res) => {
+
+ 
+  try {
+    const prompt = req.body.prompt;
+
+    
+   
+
+
+    const chapGPT = async (prompt) => {
+      const response = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [{ role: "user", content: prompt }],
+      });
+      res.status(200).send({
+        bot: response["data"]["choices"][0]["message"]["content"]
+      });
+
+      console.log(response["data"]["choices"][0]["message"]["content"]);
+      };
+    
+    chapGPT(prompt)
+   
+
+  } catch (error) {
+    console.error(error)
+    res.status(500).send(error || 'Something went wrong');
+  }
+})
+
+app.listen(5000, () => console.log('AI server started on http://localhost:5000'))
